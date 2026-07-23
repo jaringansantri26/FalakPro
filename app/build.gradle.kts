8e7d5b4e84d2bda18904a7fd9260f93f31f4d4cc
jaringansantri26 <jaringansantri@gmail.com>
2026-07-23 14:24:04 +0700

    Revert versionCode to 2 for version 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.falak.falakpro"
         minSdk = 26
         targetSdk = 37
-        versionCode = 3
-        versionName = "1.0.1"
+        versionCode = 2
+        versionName = "1.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 

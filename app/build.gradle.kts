aeca758a83d552ac7a0f1ac349255ac2282b75aa
jaringansantri26 <jaringansantri@gmail.com>
2026-07-23 14:45:19 +0700

    Update versionCode to 3 and versionName to 0.0.1 Beta

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.falak.falakpro"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.0"
+        versionCode = 3
+        versionName = "0.0.1 Beta"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
